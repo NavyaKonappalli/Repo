fbe4ae761de708de52d44db76eae4640b2381129
madhupk963 <madhusudhanpk.madhu@gmail.com>
2022-11-17 17:29:57 +0000

    1st commit by madhu

--- Fib.sh
+++ Fib.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-echo "Enter the value"
-read n
-a=0
+echo "Enter the value/number"
+read n #hello
+a=0 #this is comment
 b=1
-i=1
+i=1 #again comment
 while [ $i -le $n ]
 do
 	echo "\n$a"
         N=$((a+b))
         a=$b
         b=$N
-        i=$((i+1))
-done
+        i=$((i+1)) #its reverse
+done 
+#done for the day
